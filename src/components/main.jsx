d17171925e9f2e2ba46c953d3134fb27dbb322d2
Dom Charlesworth <dgc336@gmail.com>
2018-01-12 15:34:39 +0000

    📎 `.eslintrc` can't have .json on the end!

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,13 +2,11 @@ import { h } from 'preact'
 import './main.scss'
 
 import HueRotateTitle from './hue-rotate-title'
-import { Button } from 'ustyle-react'
 
 const main = () => (
   <div className='home__container'>
     <div className='home__title'>
       <HueRotateTitle animate>Hello World!</HueRotateTitle>
-      <Button size='large' variant='primary' blocked />
     </div>
   </div>
 )
