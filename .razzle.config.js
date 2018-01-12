0a1e85b328c7143716b61fde2894b1b133542718
Dom Charlesworth <dgc336@gmail.com>
2018-01-12 11:30:33 +0000

    🖌 Add in `cssnano` to properly minimize the css

--- .razzle.config.js
+++ .razzle.config.js
@@ -1,35 +1,38 @@
 const autoprefixer = require('autoprefixer')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = (base, { target, dev }, webpack) => {
   const config = Object.assign({}, base)
   const isServer = target !== 'web'
 
-  const postCssLoader = {
-    loader: 'postcss-loader',
-    options: {
-      ident: 'postcss',
-      plugins: () => [
-        autoprefixer({ browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'] })
-      ]
+  const loaders = {
+    sass: { loader: 'sass-loader' },
+    css: { loader: 'css-loader',  options: { modules: false, sourceMap: true, minimize: true } },
+    postCss: {
+      loader: 'postcss-loader',
+      options: {
+        ident: 'postcss',
+        plugins: () => [
+          require('autoprefixer')(),
+          require('cssnano')()
+        ]
+      }
     }
   }
 
-  const cssLoader = (() => {
-    switch (true) {
-      case isServer: return 'css-loader';
-      case dev: return ['style-loader', {loader: 'css-loader', options: {modules: false, sourceMap: true,},}, postCssLoader, 'sass-loader']
-      default: return ExtractTextPlugin.extract({
+  config.module.rules.push({
+    test: /.scss$/,
+    use: (() => {
+      if (isServer) return [ loaders.css, loaders.sass, loaders.postCss ]
+      if (dev) return ['style-loader', loaders.css, loaders.sass, loaders.postCss ]
+      
+      return ExtractTextPlugin.extract({
         fallback: 'style-loader',
-        use: [ { loader: 'css-loader', options: { importLoaders: 1 } }, postCssLoader, 'sass-loader' ]
+        use: [ { loader: 'css-loader', options: { importLoaders: 1 } }, loaders.postCss, loaders.sass ]
       })
-    }
-  })()
-
-  config.module.rules.push({ test: /.scss$/, use: cssLoader })
-  // config.plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: true, analyzerPort: isServer ? 8080 : 8081  }))
-
+    })()
+  })
+  
   !isServer && !dev && config.plugins.push(new ExtractTextPlugin('static/css/[name].[contenthash:8].css'))
 
   return config
